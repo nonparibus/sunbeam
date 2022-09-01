709f038cd6234810ea4715537961d2499499d978
pomdtr <achille.lacoin@dailymotion.com>
2022-09-01 15:32:08 +0200

    update window config

--- main.go
+++ main.go
@@ -16,12 +16,18 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:            "raycast-linux",
-		Width:            1024,
-		Height:           768,
-		Assets:           assets,
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		Title:         "raycast-linux",
+		Width:         1024,
+		Height:        768,
+		Assets:        assets,
+		Frameless:     true,
+		DisableResize: true,
+
+		AlwaysOnTop: true,
+		MaxWidth:    750,
+		MaxHeight:   475,
+
+		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
 		},
