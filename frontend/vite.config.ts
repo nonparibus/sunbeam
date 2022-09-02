f8898c6d116ce7a12a6615e810777cbf5c444e1e
pomdtr <achille.lacoin@gmail.com>
2022-09-02 23:08:13 +0000

    fix history fallback

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
+
+const rewriteSlashToIndexHtml = () => {
+  return {
+    name: 'rewrite-slash-to-index-html',
+    apply: 'serve' as "serve",
+    enforce: 'post' as "post",
+    configureServer(server) {
+      // rewrite / as index.html
+      server.middlewares.use('/', (req, _, next) => {
+        if (req.url === '/') {
+          req.url = '/index.html'
+        }
+        next()
+      })
+    },
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), rewriteSlashToIndexHtml()],
   publicDir: "./public",
+  appType: "mpa",
   build: {
     sourcemap: true,
   },
